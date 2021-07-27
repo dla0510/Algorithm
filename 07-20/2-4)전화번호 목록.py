c0e9d05d20629bde3572426716f92c5025f5aa44
dla0510 <dla0510@naver.com>
2021-07-27 21:18:19 +0900

    updated

---
+++ 07-20/2-4)전화번호 목록.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 21:15:33 2021
+
+@author: dla05
+"""
+
+'''
+2-4)
+baekjoon 5052. 전화번호 목록
+'''
+
+t = int(input())
+
+def check():
+    for i in range(len(a)-1):
+        if a[i] == a[i+1][0:len(a[i])]:
+            print('NO')
+            return
+    print('YES')
+
+for _ in range(t):
+    n = int(input())
+    a = []
+    for i in range(n):
+        a.append(input().strip())
+    a.sort()
+    check()
