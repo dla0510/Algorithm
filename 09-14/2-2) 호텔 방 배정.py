969093416b1e88a35ed5471912e6859c0579edf1
dla0510 <dla0510@naver.com>
2021-09-14 21:25:36 +0900

    updated

--- 09-14/2-2) 호텔 방 배정.py
+++ 09-14/2-2) 호텔 방 배정.py
@@ -5,3 +5,22 @@ Created on Tue Sep 14 20:57:34 2021
 @author: dla05
 """
 
+'''
+2-2) 호텔 방 배정
+
+'''
+
+def solution(k, room_number):
+    answer = []
+    room = [False]*(k+1)
+    for n in room_number:
+        if not room[n]:
+            room[n]=True
+            answer.append(n)
+        else:
+            for i in range(n+1,k+1):
+                if not room[i]:
+                    room[i]=True
+                    answer.append(i)
+                    break
+    return answer
